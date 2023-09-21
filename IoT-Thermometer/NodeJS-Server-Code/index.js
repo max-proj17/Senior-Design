a5f475477ea697cf7f88be2e5cd7c2246051f1b3
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-09-21 17:12:45 -0500

    Optimized the server to recieve data faster!

--- IoT-Thermometer/NodeJS-Server-Code/index.js
+++ IoT-Thermometer/NodeJS-Server-Code/index.js
@@ -2,56 +2,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 const mqtt = require('mqtt');
-
 const app = express();
-const port = 3000;
 
-let lastSentTemperature = null;
-let lastSentTime = null;
+let lastAlertTime = null;
+
+// User-defined settings
+const maxTemperature = 35;
+const minTemperature = -5;
+const phoneNumber = '';
+const port = 3000;  // Define the port here
+
+// Twilio setup
+const accountSid = '';
+const authToken = '';
+const twilioClient = new twilio(accountSid, authToken);
+
 // MQTT setup
 const mqttClient = mqtt.connect('mqtt://test.mosquitto.org');
 
 mqttClient.on('connect', function () {
-  console.log('Connected to MQTT broker');
-  mqttClient.subscribe('pico/temperature', { qos: 1 });
+    console.log('Connected to MQTT broker');
+    mqttClient.subscribe('pico/temperature', { qos: 1 });
 });
 
 mqttClient.on('message', function (topic, message) {
-  const temperature = parseFloat(message.toString());
-  console.log(`Received temperature: ${temperature} C`);
-  const currentTime = new Date().getTime();
+    const temperature = parseFloat(message.toString());
+    console.log(`Received temperature: ${temperature} C`);
+    const currentTime = new Date().getTime();
 
-  if (lastSentTemperature !== temperature || (lastSentTime && currentTime - lastSentTime >= 60000)) {
-    if (temperature > maxTemperature) {
-      twilioClient.messages.create({
-        body: `Warning: Temperature exceeded ${maxTemperature} C`,
-        to: phoneNumber,
-        from: 'phone-here'
-      });
-    } else if (temperature < minTemperature) {
-      twilioClient.messages.create({
-        body: `Warning: Temperature dropped below ${minTemperature} C`,
-        to: phoneNumber,
-        from: 'phone-here'
-      });
+    if ((temperature > maxTemperature || temperature < minTemperature) && 
+        (!lastAlertTime || currentTime - lastAlertTime >= 60000)) {
+        const alertMessage = temperature > maxTemperature 
+            ? `Warning: Temperature exceeded ${maxTemperature} C`
+            : `Warning: Temperature dropped below ${minTemperature} C`;
+
+        twilioClient.messages.create({
+            body: alertMessage,
+            to: phoneNumber,
+            from: ''
+        }, function(error, message) {
+            if (error) {
+                console.error("Error sending SMS:", error.message);
+            } else {
+                console.log("SMS sent with ID:", message.sid);
+            }
+        });
+
+        lastAlertTime = currentTime;
     }
-    lastSentTemperature = temperature;
-    lastSentTime = currentTime;
-  }
 });
 
-// Twilio setup
-const accountSid = 'insert here';
-const authToken = 'insert here';
-const twilioClient = new twilio(accountSid, authToken);
 
-// User-defined settings
-let maxTemperature = 35;
-let minTemperature = -5;
-let phoneNumber = 'insert here';
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
