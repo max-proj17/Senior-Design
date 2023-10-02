ac4832dc0e90630b7672465e8af92e1049b78dd2
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-10-02 18:09:31 -0500

    Removed all files

--- IoT-Thermometer/Pico-Code/lib/adafruit_displayio_layout/widgets/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Kevin Matocha, Tim C, Jose David M
-#
-# SPDX-License-Identifier: MIT
--- README.md
+++
@@ -1,2 +0,0 @@
-# Senior-Design
-Contains all of my Senior Design Software Projects
