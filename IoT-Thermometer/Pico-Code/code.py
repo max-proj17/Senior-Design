b5e3dec77097e0c6ace31a2bc8f178a6b819c1f3
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-10-01 22:46:04 -0500

    Updated Lab with entire code stack

--- IoT-Thermometer/Pico-Code/code.py
+++ IoT-Thermometer/Pico-Code/code.py
@@ -10,6 +10,7 @@ from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
 from pimoroni_pico_display import PimoroniPicoDisplay
 
+virtual_a_press = False
 # Initialize temperature sensor
 ow_bus = OneWireBus(board.GP26)
 ds18b20 = adafruit_ds18x20.DS18X20(ow_bus, ow_bus.scan()[0])
@@ -67,24 +68,52 @@ mqtt_client = MQTT.MQTT(broker=mqtt_broker, username=None, password=None, socket
 
 def connected(client, userdata, flags, rc):
     print('Connected to MQTT broker!')
+    mqtt_client.subscribe('pico/display', qos=1)  # Subscribe to the display topic
 
 mqtt_client.on_connect = connected
 mqtt_client.connect()
 
+def message_handler(client, topic, message):
+    try:
+        global virtual_a_press
+        print(f"Received message on topic {topic}: {message}")
+        if message == 'OFF':
+            print("turn off")
+            virtual_a_press = False;
+        elif message == 'ON':
+            print("turn on")
+            virtual_a_press = True;
+    except Exception as e:
+        print(f"Error handling message: {e}")
+
+
+
+            
+mqtt_client.on_message = message_handler  # Set the message handler
+
 display_on = False
 while True:
-    # Continuously read temperature and send via MQTT
-    temp = ds18b20.temperature
-    print(f'Temperature: {temp} C')
-    mqtt_client.publish('pico/temperature', str(temp), qos=1)
-    
-    # Update the display text variable and turn on/off the display based on button A state
-    if display.button_pressed('A'):
-        text_area.text = f"{temp:.2f}°C"
+    try:
+        # Continuously read temperature and send via MQTT
+        temp = ds18b20.temperature
+        print(f'Temperature: {temp} C')
+        mqtt_client.publish('pico/temperature', str(temp), qos=1)
+        #mqtt_client.loop()
+        # Update the display text variable and turn on/off the display based on button A state
+        if display.button_pressed('A') or virtual_a_press == True:
+            text_area.text = f"{temp:.2f}°C"
+            text_area.scale = 2
+            display.display.show(splash)  # Show the splash screen with the temperature
+            display.display.refresh()  # Refresh the display to show the changes
+        else:
+            display.display.show(black_screen)  # Show the black screen
+            display.display.refresh()  # Refresh the display to show the changes
+    except Exception as e:
+        # Handle sensor disconnection error
+        print("Unplugged Sensor")
+        mqtt_client.publish('pico/temperature', 'SENSOR_UNPLUGGED', qos=1)
+        text_area.text = "Unplugged"
         text_area.scale = 2
-        display.display.show(splash)  # Show the splash screen with the temperature
-        display.display.refresh()  # Refresh the display to show the changes
-    else:
-        display.display.show(black_screen)  # Show the black screen
+        display.display.show(splash)  # Show the splash screen with the error message
         display.display.refresh()  # Refresh the display to show the changes
     time.sleep(.06125)  # Sleep to avoid busy-waiting
